6662a36e5d14787f74a16bb7ad4c8280d691f207
Hardik Upadhyay <hardik.upadhyay1992@gmail.com>
2017-02-13 15:38:30 +0530

    added sprocket

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170123095128) do
+ActiveRecord::Schema.define(version: 20170201123707) do
 
   create_table "authentications", force: :cascade do |t|
     t.string   "auth_token"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20170123095128) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
